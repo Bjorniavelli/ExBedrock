1b430fa9497b007273292a9597f258a6b72b181d
Bjorniavelli <games@bjorniavelli.com>
2016-05-07 17:55:37 -0500

    Ex Hand results pull from config file.

--- src/main/java/com/bjorniavelli/exbedrock/ExBedrock.java
+++ src/main/java/com/bjorniavelli/exbedrock/ExBedrock.java
@@ -2,12 +2,17 @@ package com.bjorniavelli.exbedrock;
 
 import com.bjorniavelli.exbedrock.listener.BedrockListener;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import java.io.File;
 import java.util.logging.Logger;
@@ -26,22 +31,81 @@ public class ExBedrock
 
     public static Block exBlock;
 
+    // Drop Table
+    // Can I do this with a key/value pair system of my own??
+    public static Item[][] exHandDrops;
+    public static Item[][] exShovelDrops;
+    public static Item[][] exPickDrops;
+    public static Item[][] exAxeDrops;
+    public static Item[][] exHoeDrops;
+    public static Item[][] exShearDrops;
+    public static Item[][] exFishingDrops;
+    public static Item[][] exBucketDrops;
+    public static Item[][] exBlastDrops;
+    public static Item[][] exBurnDrops;
+
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent e)
     {
         Configuration config = new Configuration (e.getSuggestedConfigurationFile());
         config.load();
 
+        // Add Error Checking!
         String exBlockString = config.get("General", "ExBlock", "minecraft:bedrock", "Which block gets pulled from?").getString();
-        //It works for now if we give it an actual block, but I don't know how to error check that, yet.
-//        if (Block.getBlockFromName(exBlockString) != Null)
-//        {
-//            exBlock = Block.getBlockFromName(exBlockString));
-//        }
-
         exBlock = Block.getBlockFromName(exBlockString);
-        FMLLog.info (exBlock.toString() + " <--- exBlock");
+
+        // Just using this placeholder to avoid extremely long line lengths
+        // But could just as easily do exHandDrops[n] = convertStringlistToItemList(config.get...);
+        String[] s;
+
+        exHandDrops = new Item[2][];
+        s = config.getStringList("0", "Hand Drops", new String[]{"minecraft:stick"}, "Empty Hand Drops");
+        exHandDrops[0] = convertStringListToItemList(s);
+        s = config.getStringList("1", "Hand Drops", new String[]{"minecraft:planks"}, "Non Empty Hand, Non Tool Drops");
+        exHandDrops[1] = convertStringListToItemList(s);
+
         config.save();
+    }
+
+    // Converts a list of strings to their appropriate items from the registry
+    // Accepts non-existent items, returns a list of items with no nulls
+    public Item[] convertStringListToItemList (String[] sArray)
+    {
+        Item[] iArray = new Item[sArray.length];
+        for (int i = 0; i < sArray.length; i++)
+        {
+            ResourceLocation r = new ResourceLocation(sArray[i]);
+            iArray[i] = Item.itemRegistry.getObject(r);
+        }
+
+        return cleanItemArray(iArray);
+    }
+
+    // Cleans the null elements from arrays.
+    // Primarily arrays read from configs.
+    public Item[] cleanItemArray(Item[] iArray)
+    {
+        int nulls = 0;
+        for (int i = 0; i < iArray.length; i++)
+        {
+            if (iArray[i] == null)
+                nulls++;
+        }
+
+        if (nulls == 0)
+            return iArray;
+
+        Item[] ret = new Item[iArray.length - nulls];
+        int j = 0;
+        for (int i = 0; i < iArray.length; i++)
+        {
+            if (iArray[i] != null)
+            {
+                ret[j] = iArray[i];
+                j++;
+            }
+        }
+        return ret;
     }
 
     @Mod.EventHandler
--- src/main/java/com/bjorniavelli/exbedrock/listener/BedrockListener.java
+++ src/main/java/com/bjorniavelli/exbedrock/listener/BedrockListener.java
@@ -4,16 +4,21 @@ import com.bjorniavelli.exbedrock.ExBedrock;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 // I need this to be sideonly, I think.  Because it drops ghosts on the ground,
 // But Server makes nothing happen, and Client makes the ghosts still appear.  o_O
@@ -39,14 +44,47 @@ public class BedrockListener
             return;
         }
 
-        // Create the ItemStack
-        // Obviously this will need to pull from a list of possibilities.
-        ItemStack iStack = new ItemStack(Item.getByNameOrId("minecraft:stick"), 1);
-        // Create the EntityItem from it
-        EntityItem eItem = new EntityItem(e.getEntityPlayer().getEntityWorld(), e.getEntityPlayer().posX, e.getEntityPlayer().posY, e.getEntityPlayer().posZ, iStack);
-        // Spawn the item where the player is.
-        // Lots of different places to spawn it, but where the player is should prevent it from being lost in a wall of bedrock.
-        e.getEntityPlayer().getEntityWorld().spawnEntityInWorld(eItem);
+        ItemStack tool = e.getItemStack();
+        EntityPlayer player = e.getEntityPlayer();
+        World world = player.getEntityWorld();
+
+        ItemStack iStack = getDropFromTool(tool);
+//        if (tool == null)
+//        {
+//            ItemStack iStack = new ItemStack(Items.stick, 1);
+//        }
+
+        if (iStack != null)
+            world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, iStack));
     }
 
+    public ItemStack getDropFromTool(ItemStack tool)
+    {
+        if (tool == null)
+        {
+            return RandomItemStack (ExBedrock.exHandDrops[0]);
+        }
+
+        // I need to come up with how to tell what kind of tool we're using.
+        if (tool != null)
+        {
+            return RandomItemStack(ExBedrock.exHandDrops[1]);
+//            return new ItemStack (ExBedrock.exHandDrops[1][Math.rint(ExBedrock.exHandDrops[1].length)])
+        }
+
+        // This should never happen.  We have a conceptual problem.  Log it?
+        return null;
+    }
+
+    // Takes an array of Item and returns a random ItemStack from it.
+    public ItemStack RandomItemStack(Item[] iArray)
+    {
+        // does this go here or someplace more central?
+        Random r = new Random();
+
+        int index = r.nextInt(iArray.length);
+        Item i = iArray[index];
+
+        return new ItemStack(i, 1);
+    }
 }
