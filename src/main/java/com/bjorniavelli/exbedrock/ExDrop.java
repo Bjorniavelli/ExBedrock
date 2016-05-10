3c471e26baf7b917e8430bed368e407842868e07
Bjorniavelli <games@bjorniavelli.com>
2016-05-10 15:51:33 -0500

    Stuff.  Config requires some entries, but can add dynamic later. I'm terrible at -m.

--- src/main/java/com/bjorniavelli/exbedrock/ExDrop.java
+++ src/main/java/com/bjorniavelli/exbedrock/ExDrop.java
@@ -7,57 +7,43 @@ import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.fml.common.FMLLog;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 // Contains all of the info about a particular kind of drop.
 public class ExDrop
 {
     // Static refernce containing a set of all ExDrops.
     // Errmmm... do I want to do it like this?  Seems wrong, but I really only want a Set.
-    public static Set<ExDrop> exDrops = new HashSet<ExDrop>();
+    public static ArrayList<ExDrop> exDrops = new ArrayList<ExDrop>();
     public static int MAX_HARDNESS_LEVEL = 16;
 
 
     public String name;
     public Block hardness; // the block that represents its hardness level.
     public int hardnessLevel; // An ordinal representation of that hardness level.
-    public Item toolType; // Not sure how this is going to happen.
-//    public String type;  // String or Item?
-    public ArrayList<Item> drops;
+//    public teIm toolType; // Not sure how this is going to happen.
+    public String toolType;  // String or Item?
+    public ArrayList<Item> drops = new ArrayList<Item>();
     public boolean automatable; // Can this be used by machines?
     public boolean autonomous; // Does this happen on it's own?
 
-    public ExDrop(ConfigCategory config)
+    public ExDrop(int hLevel, String type, String[] exDrops)
     {
-        name = config.getName();
+        // Should we Log out what we're doing here?
+        // like, every entry gets it added in a list to the log?
 
-        // Hardness level determines what order we check for drops in.
-        // Hardness is arbitrarily limited to 16 levels.  That can be changed.
-        hardnessLevel = config.get("hardnessLevel").getInt(-1);
-        if (hardnessLevel < 0 || hardnessLevel >= MAX_HARDNESS_LEVEL)
+        if (hLevel < 0 || hLevel >= MAX_HARDNESS_LEVEL)
         {
             FMLLog.warning("Ex Bedrock: Invalid hardness level " + hardnessLevel + " Problem in config, category " + name + ".  Setting to " + (MAX_HARDNESS_LEVEL + 1) +".");
             hardnessLevel = MAX_HARDNESS_LEVEL + 1;
         }
+        hardnessLevel = hLevel;
 
+        // I don't think we can get the established list of allowed tools... :-(
+        // It's private.
+//        if (ItemTool.getToolClasses())
+        toolType = type;
 
-        // What hardness this should be able to mine goes here.
-        String block = config.get("hardness").getString();
-        hardness = Block.getBlockFromName(block);
-        if (hardness == null)
-        {
-            FMLLog.warning("Ex Bedrock: Cannot find block " + hardness + ".  Problem in config, category " + name + ".  Setting to Bedrock.");
-            hardness = Blocks.bedrock;
-        }
-
-        //
-        // Not sure how to do toolType.
-        //
-
-        String[] exdrops = config.get("exDrops").getStringList();
-        for (String s : exdrops)
+        for (String s : exDrops)
         {
             Item tempItem = Item.getByNameOrId(s);
             if (tempItem == null)
@@ -69,9 +55,51 @@ public class ExDrop
                 drops.add(tempItem);
             }
         }
-
-        // Automatable and requirePlayer are not required
-        automatable = config.get("automatable").getBoolean(true);
-        autonomous = config.get("autonomous").getBoolean(false);
     }
+
+//    public ExDrop(ConfigCategory config)
+//    {
+//        name = config.getName();
+//
+//        // Hardness level determines what order we check for drops in.
+//        // Hardness is arbitrarily limited to 16 levels.  That can be changed.
+//        hardnessLevel = config.get("hardnessLevel").getInt(-1);
+//        if (hardnessLevel < 0 || hardnessLevel >= MAX_HARDNESS_LEVEL)
+//        {
+//            FMLLog.warning("Ex Bedrock: Invalid hardness level " + hardnessLevel + " Problem in config, category " + name + ".  Setting to " + (MAX_HARDNESS_LEVEL + 1) +".");
+//            hardnessLevel = MAX_HARDNESS_LEVEL + 1;
+//        }
+//
+//
+//        // What hardness this should be able to mine goes here.
+//        String block = config.get("hardness").getString();
+//        hardness = Block.getBlockFromName(block);
+//        if (hardness == null)
+//        {
+//            FMLLog.warning("Ex Bedrock: Cannot find block " + hardness + ".  Problem in config, category " + name + ".  Setting to Bedrock.");
+//            hardness = Blocks.bedrock;
+//        }
+//
+//        //
+//        // Not sure how to do toolType.
+//        //
+//
+//        String[] exdrops = config.get("exDrops").getStringList();
+//        for (String s : exdrops)
+//        {
+//            Item tempItem = Item.getByNameOrId(s);
+//            if (tempItem == null)
+//            {
+//                FMLLog.warning("Ex Bedrock: Cannot find item " + s + ".  Problem in config, category " + name + ".  Skipping.");
+//            }
+//            else
+//            {
+//                drops.add(tempItem);
+//            }
+//        }
+//
+//        // Automatable and requirePlayer are not required
+//        automatable = config.get("automatable").getBoolean(true);
+//        autonomous = config.get("autonomous").getBoolean(false);
+//    }
 }
--- src/main/java/com/bjorniavelli/exbedrock/listener/BedrockListener.java
+++ src/main/java/com/bjorniavelli/exbedrock/listener/BedrockListener.java
@@ -1,6 +1,7 @@
 package com.bjorniavelli.exbedrock.listener;
 
 import com.bjorniavelli.exbedrock.ExBedrock;
+import com.bjorniavelli.exbedrock.ExDrop;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
@@ -19,7 +20,9 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Set;
 
 // I need this to be sideonly, I think.  Because it drops ghosts on the ground,
 // But Server makes nothing happen, and Client makes the ghosts still appear.  o_O
@@ -47,86 +50,137 @@ public class BedrockListener
 
         ItemStack tool = e.getItemStack();
         EntityPlayer player = e.getEntityPlayer();
-        World world = player.getEntityWorld();
 
-        ItemStack iStack = getDropFromTool(tool);
-//        if (tool == null)
-//        {
-//            ItemStack iStack = new ItemStack(Items.stick, 1);
-//        }
-
-        if (iStack != null)
-            world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, iStack));
-    }
-
-    public ItemStack getDropFromTool(ItemStack tool)
-    {
         if (tool == null)
         {
-            return RandomItemStack (ExBedrock.exToolDrops[0]);
+            spawnAtPlayer(player, RandomItemStack(ExBedrock.getExDrops("Unnamed", 0)));
+            return;
         }
 
-        // Doing it this way is the way to go for vanilla modding...
-        // but how to I extend it to play nicely with other mods?
-        // Also, maybe a switch would be better?
-        // Either way, there's lots of repetitive code here...
-
-        if (tool.getItem() == Items.diamond_pickaxe)
-//        if (tool.canHarvestBlock(Blocks.obsidian))
+        if (!tool.getItem().isItemTool(tool))
         {
-            return RandomItemStack(ExBedrock.exPickDrops[3]);
+            // Include logic to check for common tools, but for now...
+            spawnAtPlayer(player, RandomItemStack(ExBedrock.getExDrops("Unnamed", 1)));
         }
 
-        if (tool.getItem() == Items.iron_pickaxe)
-//        if (tool.canHarvestBlock(Blocks.diamond_ore))
-        {
-            return RandomItemStack(ExBedrock.exPickDrops[2]);
-        }
-
-        if (tool.getItem() == Items.stone_pickaxe)
-//        if (tool.canHarvestBlock(Blocks.iron_ore))
-        {
-            return RandomItemStack(ExBedrock.exPickDrops[1]);
-        }
-
-        if (tool.getItem() == Items.wooden_pickaxe)
-//        if (tool.canHarvestBlock(Blocks.cobblestone))
-        {
-            return RandomItemStack(ExBedrock.exPickDrops[0]);
-        }
-
-        if (tool.getItem() == Items.wooden_axe)
-//        if (tool.canHarvestBlock(Blocks.log))
-        {
-            return RandomItemStack(ExBedrock.exAxeDrops[0]);
-        }
-
-        // Need to add the other things, but there's gotta be a better way to do this...
-        // Shears, Hoes, Burning, etc...
-
-        // I need to come up with how to tell what kind of tool we're using.
-        if (tool != null)
-        {
-            return RandomItemStack(ExBedrock.exToolDrops[1]);
-//            return new ItemStack (ExBedrock.exHandDrops[1][Math.rint(ExBedrock.exHandDrops[1].length)])
-        }
-
-        // This should never happen.  We have a conceptual problem.  Log it?
-        return null;
+//        Item item = tool.getItem();
+//        FMLLog.info(tool.getItem() + " <----- tool.getItem()");
+//
+//        if (item == null) return;
+//
+//        FMLLog.info(item.isItemTool(tool) + " <----- .isItemTool(tool) ??");
+//
+//        Set<String> toolclass = item.getToolClasses(tool);
+//
+//        for (String s : toolclass)
+//        {
+//            FMLLog.info(s + " <---- .getToolClasses(tool) for loop");
+//            FMLLog.info(item.getHarvestLevel(tool, s) + " <---- .getHarvestLevel(tool, s) for loop");
+//        }
+//
+////        ItemStack iStack = getDropFromTool(tool);
+//////        if (tool == null)
+//////        {
+//////            ItemStack iStack = new ItemStack(Items.stick, 1);
+//////        }
+////
+////        if (iStack != null)
+////            world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, iStack));
     }
 
+//    public ItemStack getDropFromTool(ItemStack tool)
+//    {
+//        if (tool == null)
+//        {
+//            return RandomItemStack (ExBedrock.exToolDrops[0]);
+//        }
+//
+//
+//
+////        // Doing it this way is the way to go for vanilla modding...
+////        // but how to I extend it to play nicely with other mods?
+////        // Also, maybe a switch would be better?
+////        // Either way, there's lots of repetitive code here...
+////
+////        if (tool.getItem() == Items.diamond_pickaxe)
+//////        if (tool.canHarvestBlock(Blocks.obsidian))
+////        {
+////            return RandomItemStack(ExBedrock.exPickDrops[3]);
+////        }
+////
+////        if (tool.getItem() == Items.iron_pickaxe)
+//////        if (tool.canHarvestBlock(Blocks.diamond_ore))
+////        {
+////            return RandomItemStack(ExBedrock.exPickDrops[2]);
+////        }
+////
+////        if (tool.getItem() == Items.stone_pickaxe)
+//////        if (tool.canHarvestBlock(Blocks.iron_ore))
+////        {
+////            return RandomItemStack(ExBedrock.exPickDrops[1]);
+////        }
+////
+////        if (tool.getItem() == Items.wooden_pickaxe)
+//////        if (tool.canHarvestBlock(Blocks.cobblestone))
+////        {
+////            return RandomItemStack(ExBedrock.exPickDrops[0]);
+////        }
+////
+////        if (tool.getItem() == Items.wooden_axe)
+//////        if (tool.canHarvestBlock(Blocks.log))
+////        {
+////            return RandomItemStack(ExBedrock.exAxeDrops[0]);
+////        }
+////
+////        // Need to add the other things, but there's gotta be a better way to do this...
+////        // Shears, Hoes, Burning, etc...
+////
+////        // I need to come up with how to tell what kind of tool we're using.
+////        if (tool != null)
+////        {
+////            return RandomItemStack(ExBedrock.exToolDrops[1]);
+//////            return new ItemStack (ExBedrock.exHandDrops[1][Math.rint(ExBedrock.exHandDrops[1].length)])
+////        }
+//
+//        // This should never happen.  We have a conceptual problem.  Log it?
+//        return null;
+//    }
+
     // Takes an array of Item and returns a random ItemStack from it.
-    public ItemStack RandomItemStack(Item[] iArray)
+    public ItemStack RandomItemStack(ExDrop e)
     {
-        if (iArray.length == 0)
+        ArrayList<Item> iArray = e.drops;
+        // This is so horrible... we should handle this better for now.
+//        Item[] iArray = (Item[])e.drops.toArray();
+
+        if (iArray.isEmpty())
             return null;
+//        if (iArray.length == 0)
+//            return null;
 
         // does this go here or someplace more central?
         Random r = new Random();
 
-        int index = r.nextInt(iArray.length);
-        Item i = iArray[index];
+        int index = r.nextInt(iArray.size());
+        Item i = iArray.get(index);
 
         return new ItemStack(i, 1);
+    }
+
+    public void spawnAtPlayer (EntityPlayer player, ItemStack iStack)
+    {
+        if (player == null)
+        {
+            FMLLog.warning("Ex Bedrock: Tried to spawn an ItemStack at a nonexistent player.  Skipping.");
+            return;
+        }
+
+        if (iStack == null) {
+            FMLLog.warning("Ex Bedrock: Tried to spawn a non-existent ItemStack. Skipping.");
+            return;
+        }
+
+        World world = player.getEntityWorld();
+        world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, iStack));
     }
 }
